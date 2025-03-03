eac66707701f93995c1d5cb90dfafe6d90eb1824
yangx <yangxuilyx@hotmail.com>
2025-03-03 21:01:13 +0800

    更新

--- src/FastX.Identity/XIdentityModule.cs
+++ src/FastX.Identity/XIdentityModule.cs
@@ -3,6 +3,7 @@ using FastX.Modularity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenIddict.Abstractions;
 
 namespace FastX.Identity;
 
@@ -17,6 +18,27 @@ public class XIdentityModule : XModule
         services.TryAddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 
         services.AddAutoMapper(typeof(XIdentityModule).Assembly);
+
+        //services.AddOpenIddict()
+        //    .AddServer(options =>
+        //    {
+        //        options.SetTokenEndpointUris("connect/token");
+
+        //        // Enable the client credentials flow.
+        //        options.AllowClientCredentialsFlow();
+
+        //        // Register the signing and encryption credentials.
+        //        options.AddDevelopmentEncryptionCertificate()
+        //            .AddDevelopmentSigningCertificate();
+
+        //        // Register the ASP.NET Core host and configure the ASP.NET Core options.
+        //        options.UseAspNetCore()
+        //            .EnableTokenEndpointPassthrough();
+        //    })
+        //    .AddCore(options =>
+        //    {
+        //    })
+        //    ;
     }
 
     /// <summary>
@@ -25,6 +47,20 @@ public class XIdentityModule : XModule
     /// <param name="services"></param>
     public override void PostConfigureServices(IServiceCollection services)
     {
-        services.GetSingletonInstance<XSugarBuilder>().CodeFirstInitTables(typeof(XIdentityModule).Assembly);
+        services.GetSingletonInstance<XSugarBuilder>().InitTables(typeof(XIdentityModule).Assembly);
+
+        var manager = services.BuildServiceProvider().CreateScope().ServiceProvider
+            .GetRequiredService<IOpenIddictApplicationManager>();
+
+        //manager.CreateAsync(new OpenIddictApplicationDescriptor()
+        //{
+        //    ClientId = "service-worker",
+        //    ClientSecret = "388D45FA-B36B-4988-BA59-B187D329C207",
+        //    Permissions =
+        //    {
+        //        OpenIddictConstants.Permissions.Endpoints.Token,
+        //        OpenIddictConstants.Permissions.GrantTypes.ClientCredentials
+        //    }
+        //});
     }
 }
